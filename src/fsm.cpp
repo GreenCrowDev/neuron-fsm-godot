684692d4639f46127114a8365343abc7a4954d6d
GreenCrowDev <pcalifano@outlook.it>
2024-11-19 15:45:49 +0100

    Add _bind_methods implementation

--- src/fsm.cpp
+++ src/fsm.cpp
@@ -1,3 +1,6 @@
 #include "fsm.h"
 
 using namespace godot::neuron_fsm;
+
+void FSM::_bind_methods() {
+}
--- src/state.cpp
+++ src/state.cpp
@@ -1,3 +1,6 @@
 #include "state.h"
 
 using namespace godot::neuron_fsm;
+
+void State::_bind_methods() {
+}
