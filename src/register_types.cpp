57f51284109cfbeb735cb7a9bc866f4fad4da589
GreenCrowDev <pcalifano@outlook.it>
2024-11-19 14:07:34 +0100

    Add register_types

---
+++ src/register_types.cpp
@@ -0,0 +1,38 @@
+#include "register_types.h"
+
+#include <gdextension_interface.h>
+
+#include <gdextension_interface.h>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+
+#include "fsm.h"
+#include "state.h"
+
+using namespace godot;
+using namespace godot::neuron_fsm;
+
+void initialize_neuron_fsm_module(ModuleInitializationLevel p_level) {
+	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
+		GDREGISTER_CLASS(State);
+		GDREGISTER_CLASS(FSM);
+	}
+}
+
+void uninitialize_neuron_fsm_module(ModuleInitializationLevel p_level) {
+	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
+	}
+}
+
+extern "C" {
+// Initialization.
+GDExtensionBool GDE_EXPORT neuron_fsm_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+
+	init_obj.register_initializer(initialize_neuron_fsm_module);
+	init_obj.register_terminator(uninitialize_neuron_fsm_module);
+	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+
+	return init_obj.init();
+}
+}
