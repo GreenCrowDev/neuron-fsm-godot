126db99d22859eb4b002551fb30df64a8c6a9f75
GreenCrowDev <pcalifano@outlook.it>
2024-11-23 00:50:29 +0100

    Sketch gdscript interface in cube.gd file

--- demo/scripts/cube.gd
+++ demo/scripts/cube.gd
@@ -9,9 +9,8 @@ var fsm: FSM
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Hello")
+	print("ready")
 	pass
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -20,3 +19,36 @@ func _process(delta: float) -> void:
 func _do_button():
 	fsm = FSM.new()
 	print(fsm.test_func())
+
+# States declaration.
+enum ColorStates {GREEN, YELLOW, RED}
+
+class StateConnections:
+	var i
+
+func _init():
+	
+	# 1. Define states.
+	
+	var green_state: State = State.new()
+	# Use state signals and lambas to define state functions.
+	green_state.on_enter.connect(func(): print("Green!"))
+	
+	var yellow_state: State = State.new()
+	var red_state: State = State.new()
+	
+	# 2. Define states transitions.
+	
+	var green_state_pair = StateConnections.new(green_state)
+	var yellow_state_pair = StateConnections.new(yellow_state)
+	var red_state_pair = StateConnections.new(red_state)
+	
+	green_state_connections.add_connection(gsevent1, yellow_state_pair)
+	
+	yellow_state_connections.add_connection(ysevent2, red_state_pair)
+	
+	red_state_connections.add_connection(rsevent3, green_state_pair)
+	
+	# 3. Initialize finite state machine.
+	
+	fsm = FSM.new(green_state_pair)
